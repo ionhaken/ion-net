9c8801e9cfde77322bce91f6ed5f52efb08e1b3e
shaken <maakush@gmail.com>
2023-04-16 14:08:57 +0300

    update subtree

--- scripts/update_subtree.bat
+++ scripts/update_subtree.bat
@@ -1,1 +1,3 @@
+cd ..
+git subtree add --prefix depend/ion https://github.com/ionhaken/ion.git main --squash
 git subtree pull --prefix depend/ion https://github.com/ionhaken/ion.git master --squash

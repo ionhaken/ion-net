3a80364383353e64deaf233742b35ef44175e9d8
shaken <maakush@gmail.com>
2023-04-16 15:00:02 +0300

    Fix parent header file visibility

--- scripts/update_subtree.bat
+++
@@ -1,3 +0,0 @@
-cd ..
-git subtree add --prefix depend/ion https://github.com/ionhaken/ion.git main --squash
-git subtree pull --prefix depend/ion https://github.com/ionhaken/ion.git master --squash
